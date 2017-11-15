dc36f7bba8a18ffea83f15f7b7dbee9b2c30c59b
tipra2614 <iit2017088@iiita.ac.in>
2017-11-15 14:22:18 +0530

    Second commit

--- 1.py
+++ 1.py
@@ -1,14 +1,2 @@
-n=int(input())
-sum=0
-for _ in range(n):
-    t=int(input())
-    sum+=t
-print(sum)
-print(sum)
-print(sum)
-print(sum)
-print(sum)
-print(sum)
-print(sum)
-print(sum)
-    
+a=[1,3,5,2]
+print(a)
