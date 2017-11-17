a2f8431ad482d4b845fe239c8edef33c86d4384f
tipra2614 <iit2017088@iiita.ac.in>
2017-11-18 01:53:59 +0530

    Commit now

--- t4.c
+++ t4.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include<curses.h>
+#include <curses.h>
+#include <string.h>
 int main()
 {
-	FILE *f;
-	char ch[1000];
-	f=fopen("test.txt","w");
-	fgets(ch,1000,f);
-	fclose(f);
+	int *p;
 
 }
--- test2.c
+++ test2.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
+#include<string.h>
+
+int merge(int arr[],int l,int m,int r)
+{
+	int a=l,b=m+1,i,c[100];
+	for(i=l;a<=m,b<=r;i++)
+	{
+		if(arr[a]<arr[b])
+			c[i++]=arr[a++];
+		else
+			c[i++]=arr[b++];
+	}
+	while(a<=m)
+		c[i++]=arr[a++];
+	while(b<=r)
+		c[i++]=arr[b++];
+	for(int j=l;j<=r;j++)
+		arr[j]=c[j];
+}
+
+int merge_sort(int arr[],int l,int r)
+{
+	if(l<r)
+	{
+		int m=(l+r)/2;
+		merge_sort(arr,l,m);
+		merge_sort(arr,m+1,r);
+		merge(arr,l,m,r);
+	}
+	else
+		return 0;
+}
 
 int main()
 {
-   int t,n;
-   scanf("%d",&t);
-   for(int i=0;i<t;i++)
-   {
-   		scanf("%d",&n);
-   		int a=1;
-   		long int b=1,sum=0,summation=1;
-   		for(int j=0;j<n-2;j++)
-   		{
-   			sum=a+b;
-   			a=b;
-   			b=sum;
-   			if(summation>1000000007)
-   				summation=summation%1000000007;
-   			summation*=sum;
-   		}
-   		printf("%ld\n",summation);
-
-
-   }
- }
+	int arr[]={0,2,66,2,4,22,1,53,13};
+	merge_sort(arr,0,8);
+	for(int i=0;i<9;i++)
+		printf("%d ",*(arr+i));
+}
 
 
