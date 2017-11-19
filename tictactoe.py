1882d10d4c58218e5e846a696b5fc4a04691d7aa
tipra2614 <iit2017088@iiita.ac.in>
2017-11-20 02:11:46 +0530

    Commit

--- tictactoe.py
+++ tictactoe.py
@@ -8,7 +8,6 @@ while end=='n' or k<=5:
     a[x][y]=1
     flag=1
     if flag==1 and end=='n':
-        print("fun 1")
         for i in range(3):
             if flag==1:
                 try:
@@ -17,18 +16,19 @@ while end=='n' or k<=5:
                             if a[i][k]!=1:
                                 a[i][k]='a'
                                 flag=0
-                                print(k)
                     elif sum(a[i][j] for j in range(3))==3:
                         print("You Won")
                         end='yes'
                 except:
                     if a[i][0]=='a' and a[i][1]=='a' and a[i][2]=='a':
                         print("Computer Won")
-                    pass
+                        flag=0
+                        end='yes'
+                    else:
+                        pass
             else:
                 break
     if flag==1 and end=='n':
-        print("fun 2")
         for i in range(3):
             if flag==1:
                 try:
@@ -37,16 +37,19 @@ while end=='n' or k<=5:
                             if a[k][i]!=1:
                                 a[k][i]='a'
                                 flag=0
-                                print(k)
                     elif sum(a[j][i] for j in range(3))==3:
                         print("You Won")
                         end='yes'
                 except:
-                    pass
+                    if a[0][i]=='a' and a[1][i]=='a' and a[2][i]=='a':
+                        print("Computer Won")
+                        flag=0
+                        end='yes'
+                    else:
+                        pass
             else:
                 break
     if flag==1 and end=='n':
-        print("fun 3")
         try:
             if a[0][0]+a[1][1]+a[2][2]==2:
                 for i in range(3):
@@ -58,11 +61,14 @@ while end=='n' or k<=5:
                     end='yes'
             
         except:
-            flag=1
-            pass
+            if a[0][0]=='a' and a[1][1]=='a' and a[2][2]=='a':
+                print("Computer Won")
+                flag=0
+                end='yes'
+            else:
+                pass
 
     if flag==1 and end=='n':
-        print("fun 4")
         try:
             if a[0][2]+a[1][1]+a[2][0]==2:
                 for i in range(3):
@@ -70,32 +76,41 @@ while end=='n' or k<=5:
                         a[i][i]='a'
                         flag=0
             elif a[0][2]+a[1][1]+a[2][0]==3:
-                        print("You Won")
-                        end='yes'
+                print("You Won")
+                end='yes'
         except:
-            pass
+            if a[i][0]=='a' and a[i][1]=='a' and a[i][2]=='a':
+                print("Computer Won")
+                flag=0
+                end='yes'
+            else:
+                pass
 
     if flag==1:
-        print("fun 5")
         s=[]
         for i in range(3):
             for j in range(3):
                 if a[i][j]==0:
                     s+=[[i,j]]
-        n=random.randint(0,len(s)-1)
+        
         if len(s)==0:
             print("You Won")
             end='yes'
 
         else: 
-            print(s)
+            n=random.randint(0,len(s)-1)
             a[s[n][0]][s[n][1]]='a'
     k+=1
 
 
     for i in range(3):
         for j in range(3):
-            print(a[i][j],end=' ')
+            if a[i][j]=='a':
+                print('O',end=' ')
+            elif a[i][j]==1:
+                print('X',end=' ')
+            else:
+                print('*',end=' ')
         print('')
 
 
